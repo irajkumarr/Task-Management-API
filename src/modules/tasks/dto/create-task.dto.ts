e659998bdee6c27f9118fed2dce0a71b0ccda734
Raj Kumar Timalsina <rajkumar.pc009@gmail.com>
2026-06-01 16:33:11 +0545

    feat: Add CreateTaskDto and UpdateTaskDto with validation rules for task properties

--- src/modules/tasks/dto/create-task.dto.ts
+++ src/modules/tasks/dto/create-task.dto.ts
@@ -1,1 +1,31 @@
-export class CreateTaskDto {}
+import {
+  IsDate,
+  IsDateString,
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+import { TaskPriority } from '../entities/task.entity';
+
+export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  title!: string;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsEnum(TaskPriority)
+  @IsOptional()
+  priority?: TaskPriority;
+
+  @IsDateString()
+  @IsOptional()
+  dueDate?: string;
+
+  @IsString() // userId of the assignee
+  @IsOptional()
+  assigneeId?: string;
+}
--- src/modules/tasks/dto/update-task.dto.ts
+++ src/modules/tasks/dto/update-task.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTaskDto } from './create-task.dto';
+import { TaskStatus } from '../entities/task.entity';
+import { IsEnum, IsOptional } from 'class-validator';
 
-export class UpdateTaskDto extends PartialType(CreateTaskDto) {}
+export class UpdateTaskDto extends PartialType(CreateTaskDto) {
+  @IsEnum(TaskStatus)
+  @IsOptional()
+  status?: TaskStatus;
+}
